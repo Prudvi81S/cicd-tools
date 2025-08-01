38c0d3009d19bc0a2526eba347b52d79cd9f64b2
prudviraj <prudvirajvemu316@gmail.com>
2025-08-01 11:22:18 +0530

    user profile

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "zone_name" {
   description = "description"
 }
 
-variable "root_block_device" {
-    description = "description"
-    type = object({
-        volume_size = number
-        volume_type = string
-        delete_on_termination = bool
-    })
-}
+# variable "root_block_device" {
+#     description = "description"
+#     type = object({
+#         volume_size = number
+#         volume_type = string
+#         delete_on_termination = bool
+#     })
+# }
