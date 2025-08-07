6c41a53ddb608fb960a611df3ab5db75a5d28765
prudviraj <prudvirajvemu316@gmail.com>
2025-08-07 21:07:35 +0530

    user profile

--- variables.tf
+++ variables.tf
@@ -3,12 +3,3 @@ variable "zone_name" {
   default     = "paws81s.site"
   description = "description"
 }
-
-# variable "root_block_device" {
-#     description = "description"
-#     type = object({
-#         volume_size = number
-#         volume_type = string
-#         delete_on_termination = bool
-#     })
-# }
