2cde60118f98d6553abbe99a20c209786f2b2df0
prudviraj <prudvirajvemu316@gmail.com>
2025-08-08 13:26:06 +0530

    user profile

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "jenkins" {
   }
 
   # Define the root volume size and type
-  root_block_device {
+  root_block_device = {
     volume_size           = 50    # Size of the root volume in GB
     volume_type           = "gp3" # General Purpose SSD (you can change it if needed)
     delete_on_termination = true  # Automatically delete the volume when the instance is terminated
@@ -38,7 +38,7 @@ module "jenkins_agent" {
     Name = "jenkins-agent"
   }
 
-  root_block_device {
+  root_block_device = {
     volume_size           = 50    # Size of the root volume in GB
     volume_type           = "gp3" # General Purpose SSD (you can change it if needed)
     delete_on_termination = true  # Automatically delete the volume when the instance is terminated
