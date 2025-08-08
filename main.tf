09081c05262317aae33e8403ec537aaa9a795b6d
prudviraj <prudvirajvemu316@gmail.com>
2025-08-08 15:57:05 +0530

    user profile

--- main.tf
+++ main.tf
@@ -13,39 +13,37 @@ module "jenkins" {
   }
 
   # Define the root volume size and type
-  root_block_device = [
-    {
-      # device_name           = "/dev/xvda"
-      volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-  ]
-}
-
-module "jenkins_agent" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "jenkins-agent"
-
-  instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0a78c17f0e137e978"]   #replace your SG
-  subnet_id              = "subnet-0a6f144079a68fb90" #replace your Subnet
-  ami                    = data.aws_ami.ami_info.id
-  user_data              = file("jenkins-agent.sh")
-  tags = {
-    Name = "jenkins-agent"
+  root_block_device ={
+    device_name           = "/dev/xvda"
+    volume_size = 50       # Size of the root volume in GB
+    volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+    delete_on_termination = true  # Automatically delete the volume when the instance is terminated
   }
-
-  root_block_device = [
-    {
-      device_name           = "/dev/xvda"
-      volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-  ]
 }
+
+# module "jenkins_agent" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "jenkins-agent"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-0a78c17f0e137e978"]   #replace your SG
+#   subnet_id              = "subnet-0a6f144079a68fb90" #replace your Subnet
+#   ami                    = data.aws_ami.ami_info.id
+#   user_data              = file("jenkins-agent.sh")
+#   tags = {
+#     Name = "jenkins-agent"
+#   }
+
+#   root_block_device = [
+#     {
+#       device_name           = "/dev/xvda"
+#       volume_size = 50       # Size of the root volume in GB
+#       volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+#       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+#     }
+#   ]
+# }
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
